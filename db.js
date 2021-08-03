0ce5335b47c86dc36d0dcb550c6bd0b3a0fef9f7
Andy Gao <andygao1990@gmail.com>
2021-08-03 19:53:57 -0400

    first commit

---
+++ db.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+const data = [
+    {
+      name: 'LinkedIn',
+      URL: 'http://www.linkedin.com',
+      category: 'jobs'
+    },
+    {
+      name: 'Indeed',
+      URL: 'http://www.indeed.com',
+      category: 'jobs'
+    },
+    {
+      name: 'Amazon',
+      URL: 'http://www.amazon.com',
+      category: 'shopping'
+    },
+    {
+      name: 'W3C Shools - Javascript',
+      URL: 'https://www.w3schools.com/jsref/default.asp',
+      category: 'coding'
+    },
+    {
+      name: 'Target',
+      URL: 'http://www.shopping.com',
+      category: 'shopping'
+    },
+    {
+      name: 'The Weeknd',
+      URL: 'https://www.theweeknd.com/',
+      category: 'music'
+    },
+    {
+      name: 'Stack Overflow',
+      URL: 'https://stackoverflow.com/',
+      category: 'coding'
+    },
+  ];
